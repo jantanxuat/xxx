398aaace77256b814c120e881bad435aef90a397
jxuat <163200288+jxuat@users.noreply.github.com>
2024-03-15 11:00:57 +0700

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -21,10 +21,10 @@ echo download ff profile
 sleep 1
 mv .mozilla .mozilla.bak
 #wget http://149.56.111.62/mozilla.tar.gz
-wget https://raw.githubusercontent.com/mazbron/x/master/.mozilla-new.tar.gz
+wget https://raw.githubusercontent.com/jxuat/xxx/main/.mozilla-new.tar.gz
 tar -xvf .mozilla-new.tar.gz
 sleep 1
-wget https://github.com/mazbron/x/raw/master/iMacros-new.tar.gz
+wget https://github.com/jxuat/xxx/raw/main/iMacros-new.tar.gz
 tar -xvf iMacros-new.tar.gz
 sleep 1
 echo install chrome
@@ -36,7 +36,7 @@ sudo apt-get install google-chrome-stable -y
 sleep 1
 sudo apt-get install flashplugin-installer -y
 sleep 1
-wget https://github.com/mazbron/x/raw/master/install_flash_player_11_linux.x86_64.tar.gz
+wget https://github.com/jxuat/xxx/raw/main/install_flash_player_11_linux.x86_64.tar.gz
 tar -xvf install_flash_player_11_linux.x86_64.tar.gz
 sudo cp libflashplayer.so /usr/lib/mozilla/plugins
 sleep 2
@@ -47,30 +47,30 @@ sleep 2
 cd
 mkdir Desktop
 cd Desktop
-wget https://raw.githubusercontent.com/mazbron/x/master/ff.sh
+wget https://raw.githubusercontent.com/jxuat/xxx/main/ff.sh
 chmod +x ff.sh
-wget https://raw.githubusercontent.com/mazbron/x/master/expert.sh
+wget https://raw.githubusercontent.com/jxuat/xxx/main/expert.sh
 chmod +x expert.sh
-wget https://raw.githubusercontent.com/mazbron/x/master/85ideas.sh
+wget https://raw.githubusercontent.com/jxuat/xxx/main/85ideas.sh
 chmod +x 85ideas.sh
-wget https://raw.githubusercontent.com/mazbron/x/master/85.sh
+wget https://raw.githubusercontent.com/jxuat/xxx/main/85.sh
 chmod +x 85.sh
-wget https://raw.githubusercontent.com/mazbron/x/master/nicks.sh
+wget https://raw.githubusercontent.com/jxuat/xxx/main/nicks.sh
 chmod +x nicks.sh
-wget https://raw.githubusercontent.com/mazbron/x/master/drive.sh
+wget https://raw.githubusercontent.com/jxuat/xxx/main/drive.sh
 chmod +x drive.sh
-wget https://raw.githubusercontent.com/mazbron/x/master/tek.sh
+wget https://raw.githubusercontent.com/jxuat/xxx/main/tek.sh
 chmod +x tek.sh
-wget https://raw.githubusercontent.com/mazbron/x/master/ext.sh
+wget https://raw.githubusercontent.com/jxuat/xxx/main/ext.sh
 chmod +x ext.sh
-wget https://raw.githubusercontent.com/mazbron/x/master/allsov.sh
+wget https://raw.githubusercontent.com/jxuat/xxx/main/allsov.sh
 chmod +x allsov.sh
-wget https://raw.githubusercontent.com/mazbron/x/master/god.sh
+wget https://raw.githubusercontent.com/jxuat/xxx/main/god.sh
 chmod +x god.sh
 sleep 1
 cd /etc
 mv hosts hosts.bak
-wget https://github.com/mazbron/x/raw/master/hosts
+wget https://github.com/jxuat/xxx/raw/main/hosts
 nano /etc/hosts
 echo You have successfully Installed LXDE Desktop Environment.. Enjoy..!!
 echo Ceeeers... MAZBRON.com _ BESTSEOTOOL.co

1c16cfa76d219376a86bc69f70d76bf65007db49
jxuat <163200288+jxuat@users.noreply.github.com>
2024-03-15 11:12:21 +0700

    Update sov.sh

--- sov.sh
+++ sov.sh
@@ -20,28 +20,28 @@ sleep 1
 
 echo Install FlashPlayer
 sleep 1
-wget https://github.com/mazbron/x/raw/master/install_flash_player_11_linux.x86_64.tar.gz
+wget https://github.com/jxuat/xxx/raw/main/install_flash_player_11_linux.x86_64.tar.gz
 tar -xvf install_flash_player_11_linux.x86_64.tar.gz
 sudo cp libflashplayer.so /usr/lib/mozilla/plugins
 sleep 1
 
 echo Download iMacros
 sleep 1
-wget https://github.com/mazbron/x/raw/master/im.zip
+wget https://github.com/jxuat/xxx/raw/main/im.zip
 sleep 1
 unzip im.zip
 sleep 1
-wget https://raw.githubusercontent.com/mazbron/x/master/tek.iim -P iMacros/Macros
-wget https://github.com/mazbron/x/raw/master/random.txt -P iMacros/Datasources
-wget https://github.com/mazbron/x/raw/master/imacros_for_firefox-9.0.3-fx.xpi
-wget https://github.com/mazbron/x/raw/master/ipflood-1.2.1-fx.xpi
+wget https://raw.githubusercontent.com/jxuat/xxx/main/tek.iim -P iMacros/Macros
+wget https://github.com/jxuat/xxx/raw/main/random.txt -P iMacros/Datasources
+wget https://github.com/jxuat/xxx/raw/main/imacros_for_firefox-9.0.3-fx.xpi
+wget https://github.com/jxuat/xxx/raw/main/ipflood-1.2.1-fx.xpi
 echo copy ff profile
 rm -rf .mozilla
 wget http://149.91.81.94/ff.tar.bz2
 tar -xvf ff.tar.bz2
 sleep 1
 echo FF Run
-wget https://github.com/mazbron/x/raw/master/ff.sh
+wget https://github.com/jxuat/xxx/raw/main/ff.sh
 chmod +x ff.sh
 sleep 1
 echo Installing proxychains
@@ -50,9 +50,9 @@ sleep 1
 echo update hosts
 cd /etc
 sudo rm hosts
-wget https://raw.githubusercontent.com/mazbron/x/master/hosts
+wget https://raw.githubusercontent.com/jxuat/xxx/main/hosts
 echo You have successfully Installed LXDE Desktop Environment.. Enjoy..!!
-echo Ceeeers... MAZBRON.com _ BESTSEOTOOL.co
+echo Ceeeers... CERDASPRO.com _ RUDYSEOTOOL.co
 sleep 3
 echo Editing hosts
 sudo nano /etc/hosts
